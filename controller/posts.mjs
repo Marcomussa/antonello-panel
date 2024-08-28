26898d47db74747e28f2e5758507b4ebc555c12b
Marco <marcomussa567@gmail.com>
2024-08-28 17:36:43 -0300

    Add Cloudinary Functionality. To-do: Auto delete images

--- controller/posts.mjs
+++ controller/posts.mjs
@@ -1,8 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import postModel from "../db/models/Post.mjs"
 import path, { dirname } from 'path';
 import fs from 'fs';
+import { v2 as cloudinary } from 'cloudinary';
 
 const __dirname = path.resolve();
+const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = process.env
+
+cloudinary.config({
+    cloud_name: CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
+});
 
 const getAllPosts = async (req, res) => {
     try {
@@ -30,19 +40,28 @@ const searchPost = async (req, res) => {
 }
 
 const createPost = async (req, res) => {
-    const { title, description } = req.body
-    const imagePath = req.file.filename
+    try {
+        const { title, description } = req.body;
+        const image = req.file.filename;
+        const imagePath = path.join(__dirname, '/public/uploads', image);
 
-    const newPost = new postModel({
-        title,
-        image: imagePath,
-        description
-    })
+        const uploadImage = await cloudinary.uploader.upload(imagePath);
 
-    newPost.save()
+        const newPost = new postModel({
+            title,
+            image,
+            imageUrl: uploadImage.secure_url,
+            description,
+        });
 
-    res.redirect("/admin")
-}
+        newPost.save();
+
+        res.redirect("/admin");
+    } catch (error) {
+        console.error('Error al crear el post:', error);
+        res.status(500).send('Error al crear el post');
+    }
+};
 
 const updatePost = async (req, res) => {
     const { id } = req.params;
@@ -65,9 +84,9 @@ const updatePost = async (req, res) => {
 
 const updatePostImage = async (req, res) => {
     const { id, imageId } = req.params;
-    const imagePath = req.file ? req.file.filename : null;
+    const image = req.file ? req.file.filename : null;
 
-    if (!imagePath) {
+    if (!image) {
         return res.status(400).json({ error: 'No image file provided' });
     }
 
@@ -75,14 +94,19 @@ const updatePostImage = async (req, res) => {
     const oldImageFile = path.join(__dirname, '/public/uploads', post.image);
 
     try {
-        const updatedPost = await postModel.findByIdAndUpdate(id, { image: imagePath }, { new: true });
+        const uploadNewImage = await cloudinary.uploader.upload(path.join(__dirname, '/public/uploads', image));
+        const updatedPost = await postModel.findByIdAndUpdate(id, {
+            image,
+            imageUrl: uploadNewImage.secure_url
+        },
+            { new: true });
 
         if (!updatedPost) {
             return res.status(404).send('Post not found');
         }
 
         fs.access(oldImageFile, fs.constants.F_OK, (err) => {
-            if (!err) { 
+            if (!err) {
                 fs.unlink(oldImageFile, (err) => {
                     if (err) {
                         console.error('Error deleting file:', err);
@@ -108,6 +132,8 @@ const deletePost = async (req, res) => {
         if (!deletedPost) {
             return res.status(404).send('Post not found')
         }
+
+        cloudinary.uploader.destroy(imagePath);
 
         fs.access(imagePath, fs.constants.F_OK, (err) => {
             if (err) {
